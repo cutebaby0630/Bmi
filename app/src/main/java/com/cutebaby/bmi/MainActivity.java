d950424498594db96253e99a5f1888689009905f
lovemath0630@gmail.com <lovemath0630@gmail.com>
2018-10-09 15:14:07 +0800

    toast

--- app/src/main/java/com/cutebaby/bmi/MainActivity.java
+++ app/src/main/java/com/cutebaby/bmi/MainActivity.java
@@ -1,22 +1,29 @@
 package com.cutebaby.bmi;
-
+//快速鍵，區域變數變成屬性→ctrl+alt+f
+//快速鍵，整行往上→shift+alt+上鍵
+//快速鍵，顯示提示→ctrl+p
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edWeight;
+    private EditText edHeight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
     }
     public void bmi(View view){
         Log.d("MainActivity","bmi");
-        EditText edWeight = findViewById(R.id.ed_weight);
-        EditText edHeight =  findViewById(R.id.ed_height);
+//        最耗費時 →findViewById
         String w = edWeight.getText().toString();
         String h = edHeight.getText().toString();
         Log.d("MainActivity",w+"/"+h);
@@ -24,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
         float height = Float.parseFloat(h);
         float bmi = weight/(height*height);
         Log.d("MainActivity",bmi+"");
-
+//        一定要打show才會顯示
+        Toast.makeText(this,"Your  Bmi  is  "+bmi,Toast.LENGTH_LONG).show();
 
     }
+
 }
