970fe4d3d7951cf46b2cd7fd7ba1e2409ab46fdc
cutebaby0630 <lovemath0630@gmail.com>
2018-10-23 14:36:31 +0800

    Intent

--- app/src/main/java/com/cutebaby/bmi/MainActivity.java
+++ app/src/main/java/com/cutebaby/bmi/MainActivity.java
@@ -10,6 +10,7 @@ package com.cutebaby.bmi;
 //快速鍵，抽出String→alt+enter
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.strictmode.CleartextNetworkViolation;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +20,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.time.Instant;
 
 public class MainActivity extends AppCompatActivity {
        private EditText edWeight;
@@ -61,25 +64,28 @@ public class MainActivity extends AppCompatActivity {
         float weight = Float.parseFloat(w);
         float height = Float.parseFloat(h);
         float bmi = weight/(height*height);
-//        印出運算中的bmi值
+        //        印出運算中的bmi值
         Log.d("MainActivity",bmi+"");
-//        一定要打show才會顯示
-        Toast.makeText(this,getString(R.string.your_bmi_is)+bmi,Toast.LENGTH_LONG).show();
-//        設定對話框中的文字
-        new AlertDialog.Builder(this).setMessage(getString(R.string.your_bmi_is)+bmi)
-//        設定對話框中的標題
-                .setTitle(R.string.bmi)
-//       設定一個button關掉對話框，並且執行onClick中的指令
-                .setPositiveButton(R.string.ok,null)
-                .setNegativeButton(R.string.clear, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        edWeight.setText("");
-                        edHeight.setText("");
-                    }
-                })
 
-        .show();
+        Intent intent = new Intent(this,ResultActivity.class);
+        startActivity(intent);
+//        一定要打show才會顯示
+//        Toast.makeText(this,getString(R.string.your_bmi_is)+bmi,Toast.LENGTH_LONG).show();
+//      設定對話框中的文字
+//        new AlertDialog.Builder(this).setMessage(getString(R.string.your_bmi_is)+bmi)
+//        設定對話框中的標題
+//                .setTitle(R.string.bmi)
+//    設定一個button關掉對話框，並且執行onClick中的指令
+//                .setPositiveButton(R.string.ok,null)
+//                .setNegativeButton(R.string.clear, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        edWeight.setText("");
+//                        edHeight.setText("");
+//                    }
+//                })
+//
+//        .show();
     }
 
 }
